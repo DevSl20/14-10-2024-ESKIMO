98baff7954593b7d4be9469228864eb4445def85
Devang Salvi <devangsalvi21@gmail.com>
2024-10-16 13:58:32 +0000

    save changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,74 @@
+// MARK: Imports
 import Option from "./components/Option";
+
 // https://dog.ceo/api/breed/affenpinscher/images/random
 // https://dog.ceo/api/breeds/list/all
 
 const BASE_URL = `https://dog.ceo/api/`;
 
-const imageEl = document.querySelector("img");
+// === MARK: DOM Selection
 const breedListEl = document.querySelector("#data-breed-list");
+const imageEl = document.querySelector("img");
 
-// MARK: Fetch Functions
+// === MARK: Fetch
+async function getDogsList() {
+  let breeds = JSON.parse(localStorage.getItem("breeds"));
 
-function getDogsList() {
-  return fetch(`${BASE_URL}breeds/list/all`)
-    .then((res) => res.json())
-    .then((data) => data.message)
-    .catch((err) => console.error("error aali", err));
-}
-
-getDogsList();
-
-// TODO Implement this
-function getDogImage("Husky") {}
-
-// MARK: Render
-function renderSelect() {
-  getDogsList().then((breedList) => {
-    for (let breed in breedList) {
-      const option = document.createElement("option");
-      option.textContent = breed;
-      breedListEl.appendChild(option);
+  if (!breeds) {
+    try {
+      const res = await fetch(`${BASE_URL}breeds/list/all`);
+      const data = await res.json();
+      localStorage.setItem("breeds", JSON.stringify(data.message));
+      breeds = data.message;
+    } catch (err) {
+      console.error("Error occured", err);
     }
-  });
+  }
+
+  return breeds;
 }
 
-renderSelect();
-//get the dogs list
-// create multiple options tags on this list
-// const option = document.createElement("option");
-// option.textcontent = "some data";
-// option.value = "some value";
-//     // then append this option list to the select
-// breedList.appendChild(option);
-//  }
+// Fetch a single dog breed image
+async function getDogImage(breed) {
+  try {
+    const res = await fetch(`${BASE_URL}breed/${breed}/images/random`);
+    const data = await res.json();
+    return data.message;
+  } catch (error) {
+    return console.error(error);
+  }
+}
 
-function renderImage() {}
+// === MARK: Render
+async function renderSelect() {
+  const dogsList = await getDogsList();
 
-// async function getImage() {
-//   const res = await fetch(
-//     `https://dog.ceo/api/breed/affenpinscher/images/random`
-//   );
-//   const data = await res.json();
-//   console.log(data.message);
+  const fragment = document.createDocumentFragment();
 
-//   imageEl.src = data.message;
-// }
+  console.log(dogsList);
 
-// getImage();
+  Object.keys(dogsList).forEach((dogName) => {
+    fragment.appendChild(Option(dogName));
+  });
+
+  breedListEl.append(fragment);
+}
+
+async function renderImage(breed) {
+  imageEl.src = "loading.gif";
+
+  const dogImage = await getDogImage(breed);
+  imageEl.src = dogImage;
+  imageEl.alt = breed;
+}
+
+// === MARK:  Events
+breedListEl.addEventListener("change", async (e) => {
+  const currentValue = e.target.value;
+  renderImage(currentValue);
+});
+
+// === Render on inital load
+document.addEventListener("DOMContentLoaded", () => {
+  renderSelect();
+});
