55f5868f4a933fe36cec29c8c6084ae768663621
Devang Salvi <devangsalvi21@gmail.com>
2024-10-20 09:15:53 +0000

    some changes saved

--- src/js/components/SingleCarousel.js
+++ src/js/components/SingleCarousel.js
@@ -1,7 +1,10 @@
-// TODO: Alt ko proper add karna hai
 export default function SingleCarousel(src, active = false) {
   const div = document.createElement("div");
   div.classList.add("carousel-item", `${active && "active"}`);
-  div.innerHTML = `<img src="${src}" class="d-block w-100 rounded-3" alt="dog"/>`;
+  div.innerHTML = `<img
+      src="${src}"
+      class="d-block w-100 rounded"
+      alt="dogname"
+    />`;
   return div;
 }
